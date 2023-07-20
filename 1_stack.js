98d48e14ea41955b73a679839b6b14638e64019c
Cesar Aguilar <Blueavian9@gmail.com>
2023-07-19 19:48:45 -0700

    Initial commit

--- 1_stack.js
+++ 1_stack.js
@@ -9,9 +9,9 @@ const myStack = new stack();
 myStack.push(3);
 myStack.push(6);
 myStack.push(9);
-console.log(myStack.pop()); // 9
-console.log(myStack.pop()); // 6
-console.log(myStack.pop()); // 3
+console.log(myStack.pop(9)); // 9
+console.log(myStack.pop(6)); // 6
+console.log(myStack.pop(3)); // 3
 
 // Using a JavaScript array, implement a stack.
 // Hint: push/pop operations in a stack happen on the same side. Picture a stack of pancakes -- adding and removing of flapjacks always happens from the "top" of the stack. With an array, we can use visualize either the "left" or "right" side of the array as the "top" of the stack, so there are two ways to implement this.
@@ -22,9 +22,9 @@ console.log(myStack.pop()); // 3
 // pop: remove an element from the right side
 // shift: remove an element from the left side
 
-      // Interview Steps preparation
-// 1. Understand requirement of the ? 
+// Interview Steps preparation
+// 1. Understand requirement of the ?
 // 2. discuss your understanding of the ?
-// 3. discuss plan and approach, solution 
-// 4. translate solution into a pragmmatic code.
+// 3. discuss plan and approach, solution
+// 4. translate solution into a pragmatic code.
 // 5. how, what, where, when, why,
